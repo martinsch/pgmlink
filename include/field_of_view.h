8ca4519980112e16fb6de16d822a7a190bc4703f
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-02-22 17:25:46 +0100

    documented FieldOfView

--- include/field_of_view.h
+++ include/field_of_view.h
@@ -6,10 +6,15 @@
 
 namespace Tracking {
 
+  /** Field of view in 3d+t space as a rectangular cuboid. */
   class FieldOfView {
   public:
   FieldOfView() : lb_(4, 0), ub_(4, 0) {}
 
+    /**
+     * Set lower and upper bound of cuboid.
+     * One has for all coordinates l. <= u.
+     */
   FieldOfView& set_boundingbox(double lt,
 		  double lx,
 		  double ly,
@@ -24,7 +29,13 @@ namespace Tracking {
   const std::vector<double>& lower_bound() const;
   const std::vector<double>& upper_bound() const;
 
+  /**
+   * Distance to nearest face of the cuboid.
+   * The point can be inside or outside the field of view.
+   */
   double spatial_margin( double t, double x, double y, double z ) const;
+  
+  /** Shortest distance to the temporal boundary of the field of view. */
   double temporal_margin( double t, double x, double y, double z ) const;
 
   private:
