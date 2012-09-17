26feff19e8d762ee61c913029d4e25a89e2d1e31
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2012-09-17 17:04:12 +0200

    changed the neighborhood size in hypotheses graph

--- src/track.cpp
+++ src/track.cpp
@@ -162,7 +162,9 @@ vector<vector<Event> > MrfTracking::operator()(TraxelStore& ts) {
 	}
 
 	cout << "-> building hypotheses" << endl;
-	SingleTimestepTraxel_HypothesesBuilder::Options builder_opts(6, 50);
+   //cout << "WARNING: considering only a neighborhood of 3" << endl;
+	SingleTimestepTraxel_HypothesesBuilder::Options builder_opts(3, 40);
+	//SingleTimestepTraxel_HypothesesBuilder::Options builder_opts(6, 50);
 	SingleTimestepTraxel_HypothesesBuilder hyp_builder(&ts, builder_opts);
 	HypothesesGraph* graph = hyp_builder.build();
 
