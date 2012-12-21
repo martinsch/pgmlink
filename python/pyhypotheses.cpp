a1869b53e75977c3310ea7b86fe03ff92427e470
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-12-21 14:09:52 +0100

    python wrapped HypothesesGraph

--- python/pyhypotheses.cpp
+++ python/pyhypotheses.cpp
@@ -1,5 +1,8 @@
 #include <boost/python.hpp>
+#include <boost/python/return_internal_reference.hpp>
 #include <boost/utility.hpp>
+
+#include <lemon/core.h>
 
 #include "../include/pgmlink/hypotheses.h"
 
@@ -7,10 +10,65 @@ using namespace pgmlink;
 using namespace boost::python;
 
 void export_hypotheses() {
-      class_<HypothesesGraph, boost::noncopyable>("HypothesesGraph")
-      //.def("addNode", &HypothesesGraph::add_node)
-      //.def("timesteps", &HypothesesGraph::timesteps)
-      //.def("earliest_timestep", &HypothesesGraph::earliest_timestep)
-      //.def("latest_timestep", &HypothesesGraph::latest_timestep)
+  class_<typename HypothesesGraph::Arc>("Arc");
+  class_<typename HypothesesGraph::ArcIt>("ArcIt");
+  class_<typename HypothesesGraph::Node>("Node");
+  class_<typename HypothesesGraph::NodeIt>("NodeIt");
+  class_<typename HypothesesGraph::InArcIt>("InArcIt");
+  class_<typename HypothesesGraph::OutArcIt>("OutArcIt");
+
+  // handle function overloading
+  void (HypothesesGraph::*erase1)(const HypothesesGraph::Node) = &HypothesesGraph::erase;
+  void (HypothesesGraph::*erase2)(const HypothesesGraph::Arc) = &HypothesesGraph::erase;
+  bool (HypothesesGraph::*valid1)(const HypothesesGraph::Node) const = &HypothesesGraph::valid;
+  bool (HypothesesGraph::*valid2)(const HypothesesGraph::Arc) const = &HypothesesGraph::valid;
+  int (*id1)(HypothesesGraph::Node) = &HypothesesGraph::id;
+  int (*id2)(HypothesesGraph::Arc) = &HypothesesGraph::id;
+  HypothesesGraph::Node (HypothesesGraph::*baseNode1)(const HypothesesGraph::InArcIt&) const =
+    &HypothesesGraph::baseNode;
+  HypothesesGraph::Node (HypothesesGraph::*baseNode2)(const HypothesesGraph::OutArcIt&) const =
+    &HypothesesGraph::baseNode;
+  HypothesesGraph::Node (HypothesesGraph::*runningNode1)(const HypothesesGraph::InArcIt&) const =
+    &HypothesesGraph::runningNode;
+  HypothesesGraph::Node (HypothesesGraph::*runningNode2)(const HypothesesGraph::OutArcIt&) const =
+    &HypothesesGraph::runningNode;
+
+  class_<HypothesesGraph, boost::noncopyable>("HypothesesGraph")
+    .def("addNode", &HypothesesGraph::add_node)
+    //.def("timesteps", &HypothesesGraph::timesteps, 
+    //   return_internal_reference<>())
+    .def("earliest_timestep", &HypothesesGraph::earliest_timestep)
+    .def("latest_timestep", &HypothesesGraph::latest_timestep)
+
+    // lemon graph interface
+    .def("addArc", &HypothesesGraph::addArc)
+    .def("erase", erase1)
+    .def("erase", erase2)
+    .def("valid", valid1)
+    .def("valid", valid2)
+    .def("changeTarget", &HypothesesGraph::changeTarget)
+    .def("changeSource", &HypothesesGraph::changeSource)
+    .def("target", &HypothesesGraph::target)
+    .def("source", &HypothesesGraph::source)
+    .def("id", id1)
+    .def("id", id2)
+    .def("nodeFromId", &HypothesesGraph::nodeFromId)
+    .def("arcFromId", &HypothesesGraph::arcFromId)
+    .def("maxNodeId", &HypothesesGraph::maxNodeId)
+    .def("maxArcId", &HypothesesGraph::maxArcId)
+    .def("baseNode", baseNode1)
+    .def("runningNode", runningNode1)
+    .def("baseNode", baseNode2)
+    .def("runningNode", runningNode2)
+    .def("oppositeNode", &HypothesesGraph::oppositeNode)
     ;
+
+  //
+  // lemon
+  //
+  int (*countNodes)(const HypothesesGraph&) = lemon::countNodes;
+  def("countNodes", countNodes);
+
+  int (*countArcs)(const HypothesesGraph&) = lemon::countArcs;
+  def("countArcs", countArcs);
 }
--- python/tests/pgmlink_test.py
+++ python/tests/pgmlink_test.py
@@ -22,5 +22,30 @@ class Test_Traxels( ut.TestCase ):
         self.assertEqual(t.get_feature_value("com", 1), 23)
         self.assertEqual(t.get_feature_value("com", 2), 12)
 
+class Test_HypothesesGraph( ut.TestCase ):
+    def runTest( self ):
+        # exercise the interface
+
+        g = pgmlink.HypothesesGraph()
+        n1 = g.addNode(0)
+        n2 = g.addNode(5)
+        n3 = g.addNode(7)
+        a1 = g.addArc(n1, n2)
+        a2 = g.addArc(n1,n3)
+        self.assertEqual( pgmlink.countNodes(g), 3)
+        self.assertEqual( pgmlink.countArcs(g), 2)
+        self.assertEqual( g.earliest_timestep(), 0 )
+        self.assertEqual( g.latest_timestep(), 7 )
+
+        g.erase(a2)
+        g.erase(n3)
+        self.assertEqual( pgmlink.countNodes(g), 2)
+        self.assertEqual( pgmlink.countArcs(g), 1)
+        self.assertTrue( g.valid(n1) ) 
+        self.assertTrue( g.valid(n2) )
+        self.assertTrue( not g.valid(n3) ) 
+        self.assertTrue( g.valid(a1) ) 
+        self.assertTrue( not g.valid(a2) )  
+
 if __name__=="__main__":
     ut.main()
