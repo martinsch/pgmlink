9adec2176e16f96f8dc667b6ec71ec53ee6470a8
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-02-18 16:19:14 +0100

    added html to gitignore and added hanslovsky_grammar.h

---
+++ include/pgmlink/hanslovsky_grammar.h
@@ -0,0 +1,52 @@
+#ifndef HANSLOVSKY_GRAMMAR_H
+#define HANSLOVSKY_GRAMMAR_H
+
+// stl headers
+#include <vector>
+
+
+// external headers
+#include <armadillo>
+
+
+// pgmlink headers
+
+namespace pgmlink {
+
+  typedef std::vector<std::string> feature_list;
+  
+  class KMeans;
+
+  template <typename T, typename U>
+  void feature_array_to_arma_mat(const std::vector<T>& in,
+				 arma::Mat<U>& out);
+
+  template <typename T>
+  void get_centers(const arma::Mat<T>& data,
+		   const arma::Col<size_t> labels,
+		   arma::Mat<T>& centers,
+		   int k);
+
+  class FeatureExtractorBase;
+
+  class FeatureExtractorMCOMsFromPCOMs;
+
+  class FeatureExtractorMCOMsFromMCOMs;
+
+  class DistanceBase;
+
+  class DistanceFromCOMs;
+
+  class ResolveAmbiguousArcsBase;
+
+  class ResolveAmbiguousArcsGreedy;
+
+  class ResolveAmbiguousArcsPgm;
+
+  class ReasonerMaxOneArc;
+
+  class MergerResolver;  
+
+}
+
+#endif /* HANSLOVSKY_GRAMMAR_H */
