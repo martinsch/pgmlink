c9d796ea4217e71f987c428967c86d59eeb64669
Philipp Hanslovsky <phanslov@quadxeon5.iwr.uni-heidelberg.de>
2013-03-01 18:30:34 +0100

    fixed means, sigmas bug for pytrack.cpp

--- python/pytrack.cpp
+++ python/pytrack.cpp
@@ -49,7 +49,7 @@ void export_track() {
     ;
 
     class_<ConsTracking>("ConsTracking",
-			init<int,double,double,string,bool,double,double,double,bool,double,double,bool,double,double,double,
+			init<int,double,double,string,bool,double,double,double,bool,double,double,bool,double,double,
 			std::vector<double>,std::vector<double> >(
 						args("max_number_objects", "max_neighbor_distance", "division_threshold",
 							"detection_rf_filename", "size_dependent_detection_prob", "forbidden_cost",
@@ -57,7 +57,7 @@ void export_track() {
 							"with_tracklets",
 							"division_weight", "transition_weight",
 							"with_divisions",
-							"disappearance_cost", "appearance_cost", "sigma_detection","means","sigmas")))
+							"disappearance_cost", "appearance_cost","means","sigmas")))
 	  .def("__call__", &ConsTracking::operator())
 	  .def("detections", &ConsTracking::detections)
 	;
