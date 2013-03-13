033310e72f1d848a6d0a61103cb7821f95174b4a
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-03-13 19:44:38 +0100

    added n_neighbors to pyhton wrapper

--- python/pytrack.cpp
+++ python/pytrack.cpp
@@ -29,8 +29,8 @@ void export_track() {
     ;
 
     class_<ChaingraphTracking>("ChaingraphTracking", 
-			init<string,double,double,double,double,bool,double,double,bool,bool,double,double,double>(
-								     args("random_forest_filename", "appearance", "disappearance", "detection", "misdetection", "use_random_forest", "opportunity_cost", "forbidden_cost", "with_constraints", "fixed_detections", "mean_div_dist", "min_angle", "ep_gap")))
+			       init<string,double,double,double,double,bool,double,double,bool,bool,double,double,double, double>(
+							  args("random_forest_filename", "appearance", "disappearance", "detection", "misdetection", "use_random_forest", "opportunity_cost", "forbidden_cost", "with_constraints", "fixed_detections", "mean_div_dist", "min_angle", "ep_gap", "n_neighbors")))
       .def("__call__", &ChaingraphTracking::operator())
       .def("detections", &ChaingraphTracking::detections) 
     ;
