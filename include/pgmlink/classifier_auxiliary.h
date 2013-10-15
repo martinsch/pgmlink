49bd298b4b24f01f6cdc9f38aa9ad87c3ad278de
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-10-15 17:40:14 +0200

    classification auxiliaries: feature extraction
    Former-commit-id: cb6552e79312cf13117aad43de6fdf647b9d14a6

---
+++ include/pgmlink/classifier_auxiliary.h
@@ -0,0 +1,53 @@
+#ifndef CLASSIFIER_AUXILIARY_H
+#define CLASSIFIER_AUXILIARY_H
+
+// stl
+#include <vector>
+#include <string>
+
+// boost
+#include <boost/shared_ptr.hpp>
+
+// vigra
+
+// pgmlink
+#include "pgmlink/feature.h"
+#include "pgmlink/traxels.h"
+
+namespace pgmlink {
+
+class FeatureCalculator {
+ public:
+  static const std::string name;
+  static const unsigned length;
+
+  virtual ~FeatureCalculator();
+  virtual feature_array calculate(const feature_array& f1, const feature_array& f2) const = 0;
+};
+
+
+class DistanceCalculator : public FeatureCalculator {
+ public:
+  static const std::string name;
+  static const unsigned length;
+
+  virtual ~DistanceCalculator();
+  virtual feature_array calculate(const feature_array& f1, const feature_array& f2) const;
+};
+
+  
+
+class FeatureExtractor {
+ public:
+  explicit FeatureExtractor(boost::shared_ptr<FeatureCalculator> calculator, const std::vector<std::string>& feature_names);
+  feature_array extract(const Traxel& t1, const Traxel& t2) const;
+  
+ private:
+  boost::shared_ptr<FeatureCalculator> calculator_;
+  const std::vector<std::string>& feature_names_;
+};
+  
+
+} /* namesapce pgmlink */
+
+#endif /* CLASSIFIER_H */
---
+++ src/classifier_auxiliary.cpp
@@ -0,0 +1,84 @@
+// stl
+#include <vector>
+#include <string>
+#include <numeric>
+#include <cassert>
+
+// boost
+#include <boost/shared_ptr.hpp>
+
+// vigra
+
+// pgmlink
+#include "pgmlink/feature.h"
+#include "pgmlink/traxels.h"
+#include "pgmlink/classifier_auxiliary.h"
+
+
+namespace pgmlink {
+
+////
+//// class FeatureCalculator
+////
+const std::string FeatureCalculator::name = "";
+
+const unsigned FeatureCalculator::length = 0;
+
+
+FeatureCalculator::~FeatureCalculator() {
+
+}
+
+
+////
+//// class DistanceCalculator
+////
+const std::string DistanceCalculator::name = "distance";
+
+const unsigned DistanceCalculator::length = 1;
+
+
+DistanceCalculator::~DistanceCalculator() {
+
+}
+
+
+feature_array DistanceCalculator::calculate(const feature_array& f1, const feature_array& f2) const {
+  assert(f1.size() == f2.size());
+  feature_array ret(length, 0.);
+  feature_array::const_iterator f1_it = f1.begin();
+  feature_array::const_iterator f2_it = f2.begin();
+  for (; f1_it != f1.end(); ++f1_it, ++f2_it) {
+    ret[0] += (*f1_it - *f2_it)*(*f1_it - *f2_it);
+  }
+  return ret;
+}
+
+
+////
+//// class FeatureExtractor
+////
+FeatureExtractor::FeatureExtractor(boost::shared_ptr<FeatureCalculator> calculator, const std::vector<std::string>& feature_names)
+    : calculator_(calculator), feature_names_(feature_names) {
+  
+}
+
+
+feature_array FeatureExtractor::extract(const Traxel& t1, const Traxel& t2) const {
+  feature_array features1, features2;
+  for (std::vector<std::string>::const_iterator feature_name = feature_names_.begin();
+       feature_name != feature_names_.end();
+       ++feature_name) {
+    FeatureMap::const_iterator f1 = t1.features.find(*feature_name);
+    FeatureMap::const_iterator f2 = t2.features.find(*feature_name);
+    assert(f1 != t1.features.end());
+    assert(f2 != t2.features.end());
+    features1.insert(features1.end(), f1->second.begin(), f1->second.end());
+    features2.insert(features2.end(), f2->second.begin(), f2->second.end());
+  }
+  return calculator_->calculate(features1, features2);
+}
+
+
+
+} /* namespace pgmlink */
