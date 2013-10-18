2ca3e39c1eac3ab6ba29a083c11e743519411816
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-10-18 17:53:12 +0200

    added option for precomputed count classifier
    Former-commit-id: 6cc4740fd6f317a0d6f1dab6f6898206822f4170

--- include/pgmlink/multi_hypotheses_tracking.h
+++ include/pgmlink/multi_hypotheses_tracking.h
@@ -46,6 +46,7 @@ public:
     bool with_maximal_conflict_cliques;
     bool forward_backward;
     bool constant_classifier_fallback;
+    bool classifier_count_precomputed;
   };
   MultiHypothesesTracking(const Options& options) : options_(options) {}
   boost::shared_ptr<std::vector<std::vector<Event> > > operator()(MultiHypothesesTraxelStore& ts);
