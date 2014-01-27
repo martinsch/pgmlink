45ca02350956012bafcaed80a151fea2fced7e08
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2014-01-27 23:53:33 +0100

    log max cost + opportunity

--- src/pgm_multi_hypotheses.cpp
+++ src/pgm_multi_hypotheses.cpp
@@ -652,7 +652,7 @@ void CVPR2014ModelBuilder::add_conflict_factors( const MultiHypothesesGraph& hyp
       }
       table.set_value( coords, deactivated_energy_max + opportunity_cost());
       LOG(logDEBUG4) << "CVPR2014ModelBuilder::add_conflict_factors() -- maximum deactivation energy: "
-                     << deactivated_energy_max;
+                     << deactivated_energy_max + opportunity_cost();
     }
   }
 }
