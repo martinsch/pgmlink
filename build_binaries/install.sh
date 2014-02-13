c0277a10de394bd977dceb08b62a967f866cf33d
mschiegg <mail@mschiegg.de>
2014-02-13 01:02:00 +0100

    updated install.sh

--- build_binaries/install.sh
+++ build_binaries/install.sh
@@ -1,11 +1,12 @@
 ILASTIK_BUILD=$PWD
+CPLEX_ROOT_DIR=$HOME/local
 
-sudo apt-get install build-essential g++ gcc git cmake cmake-curses-gui vim gfortran libxext-dev &&
+sudo apt-get install build-essential g++ gcc git cmake cmake-curses-gui gfortran libxext-dev &&
 
 mkdir -p ilastik-build/build &&
 cd ilastik-build &&
 
-cmake -D BUILDEM_DIR=$ILASTIK_BUILD/build -D CMAKE_CXX_COMPILER=g++ -D CMAKE_C_COMPILER=gcc $ILASTIK_BUILD &&
+cmake -D BUILDEM_DIR=$ILASTIK_BUILD/build -D CMAKE_CXX_COMPILER=g++ -D CMAKE_C_COMPILER=gcc -D CMAKE_CPLEX_ROOT_DIR=$CPLEX_ROOT_DIR $ILASTIK_BUILD &&
 
 make &&
 
