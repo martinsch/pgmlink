5fa7ca898b66f549847715a3312e41444f9be9f6
dstoe <stockel.david@gmail.com>
2014-03-12 14:43:49 +0100

    Moved the class "OutlierCalculator" into "track_features_auxiliary" files. Now I can implement the OutlierCalculator as a FeatureAggregator as well.

--- include/pgmlink/outlier_detection.h
+++ include/pgmlink/outlier_detection.h
@@ -7,10 +7,6 @@
 #define PGMLINK_OUTLIER_DETECTION_H
 
 #include <vector>
-#include <stdexcept>
-#include <cassert>
-
-#include <armadillo>
 
 // pgmlink
 #include "pgmlink/tracks.h"
@@ -21,39 +17,6 @@
 #include <boost/shared_ptr.hpp>
 
 namespace pgmlink {
-
-class OutlierCalculator {
-  public:
-    OutlierCalculator() {};
-    ~OutlierCalculator() {};
-    virtual const std::vector<size_t>& calculate(const feature_arrays& features) = 0;
-    virtual const feature_array& get_measures() const;
-    virtual const std::string& name() const;
-  protected:
-    static const std::string name_;
-    feature_array measures_;
-}; // class OutlierCalculator
-
-class MVNOutlierCalculator : public OutlierCalculator {
-  public:
-    MVNOutlierCalculator() {};
-    ~MVNOutlierCalculator() {};
-    const std::vector<size_t>& calculate(const feature_arrays& features);
-    const feature_array& get_measures() const;
-    const arma::Mat<feature_type>& get_covariance() const;
-    const arma::Mat<feature_type>& get_inverse_covariance() const;
-    const arma::Col<feature_type>& get_mean() const;
-    virtual const std::string& name() const;
-
-  protected:
-    static const std::string name_;
-    static const feature_type sigma_threshold_;
-    feature_array measures_;
-    std::vector<size_t> outlier_ids_;
-    arma::Col<feature_type> mean_;
-    arma::Mat<feature_type> covariance_;
-    arma::Mat<feature_type> inv_covariance_;
-}; // class MVNOutlierCalculator
 
 class Outlier {
   public:
--- include/pgmlink/track_features_auxiliary.h
+++ include/pgmlink/track_features_auxiliary.h
@@ -1,11 +1,49 @@
 #ifndef PGMLINK_TRACK_FEATURES_H
 #define PGMLINK_TRACK_FEATURES_H
 
+#include <cassert>
+#include <stdexcept>
 #include <vector>
+#include <armadillo>
 
 #include "pgmlink/traxels.h"
 
 namespace pgmlink {
+
+class OutlierCalculator {
+  public:
+    OutlierCalculator() {};
+    ~OutlierCalculator() {};
+    virtual const std::vector<size_t>& calculate(const feature_arrays& features) = 0;
+    virtual const feature_array& get_measures() const;
+    virtual const std::string& name() const;
+  protected:
+    static const std::string name_;
+    feature_array measures_;
+}; // class OutlierCalculator
+
+class MVNOutlierCalculator : public OutlierCalculator {
+  public:
+    MVNOutlierCalculator() {};
+    ~MVNOutlierCalculator() {};
+    const std::vector<size_t>& calculate(const feature_arrays& features);
+    const feature_array& get_measures() const;
+    const arma::Mat<feature_type>& get_covariance() const;
+    const arma::Mat<feature_type>& get_inverse_covariance() const;
+    const arma::Col<feature_type>& get_mean() const;
+    virtual const std::string& name() const;
+
+  protected:
+    static const std::string name_;
+    static const feature_type sigma_threshold_;
+    feature_array measures_;
+    std::vector<size_t> outlier_ids_;
+    arma::Col<feature_type> mean_;
+    arma::Mat<feature_type> covariance_;
+    arma::Mat<feature_type> inv_covariance_;
+}; // class MVNOutlierCalculator
+
+
 
 class FeatureAggregator {
  public:
