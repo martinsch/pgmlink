babc2b42f2642abb456d0570349c268257b78f43
dstoe <stockel.david@gmail.com>
2014-03-13 11:11:01 +0100

    Added test for "MeanAggregator"

--- tests/tracks_auxiliary_test.cpp
+++ tests/tracks_auxiliary_test.cpp
@@ -134,3 +134,43 @@ BOOST_AUTO_TEST_CASE( MaxAggregator_test ) {
 
 }
 
+BOOST_AUTO_TEST_CASE( MeanAggregator_test ) {
+  // Set up some test data
+  feature_type setI_array[3][2] = {
+    {0., 1.},
+    {4., 8.},
+    {2., 3.}
+  };
+  feature_arrays setI;
+  for(size_t i = 0; i < 3; i++) {
+    feature_array y(setI_array[i], setI_array[i]+2);
+    setI.push_back(y);
+  }
+  feature_type setII_array[2][1] = {{0}, {2}};
+  feature_arrays setII;
+  for(size_t i = 0; i < 2; i++) {
+    feature_array y(setII_array[i], setII_array[i]+1);
+    setII.push_back(y);
+  }
+
+  MeanAggregator mean;
+  feature_array vectorI = mean.vector_valued(setI);
+  feature_type scalarI = mean.scalar_valued(setI);
+
+  feature_array vectorII = mean.vector_valued(setII);
+  feature_type scalarII = mean.scalar_valued(setII);
+
+  feature_array vectorIref(2); vectorIref[0] = 2.; vectorIref[1] = 4.;
+  feature_type scalarIref = 3.;
+
+  feature_array vectorIIref(1); vectorIIref[0] = 1.;
+  feature_type scalarIIref = 1.;
+
+  BOOST_CHECK_EQUAL(vectorIref[0], vectorI[0]);
+  BOOST_CHECK_EQUAL(vectorIref[1], vectorI[1]);
+  BOOST_CHECK_EQUAL(scalarIref, scalarI);
+  
+  BOOST_CHECK_EQUAL(vectorIIref[0], vectorII[0]);
+  BOOST_CHECK_EQUAL(scalarIIref, scalarII);
+
+}
