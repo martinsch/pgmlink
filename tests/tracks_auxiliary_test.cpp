d2a3e637db797199a6e25624dc054468e3b9ca08
dstoe <stockel.david@gmail.com>
2014-03-13 11:06:29 +0100

    Added tests for "MinAggregator" and "MaxAggregator"

--- tests/tracks_auxiliary_test.cpp
+++ tests/tracks_auxiliary_test.cpp
@@ -51,3 +51,86 @@ BOOST_AUTO_TEST_CASE( TotalDiffAggregator_test ) {
   BOOST_CHECK_EQUAL(scalarIIref, scalarII);
 
 }
+
+BOOST_AUTO_TEST_CASE( MinAggregator_test ) {
+  // Set up some test data
+  feature_type setI_array[3][2] = {
+    {0., 1.},
+    {4., 8.},
+    {2., 3.}
+  };
+  feature_arrays setI;
+  for(size_t i = 0; i < 3; i++) {
+    feature_array y(setI_array[i], setI_array[i]+2);
+    setI.push_back(y);
+  }
+  feature_type setII_array[2][1] = {{0}, {2}};
+  feature_arrays setII;
+  for(size_t i = 0; i < 2; i++) {
+    feature_array y(setII_array[i], setII_array[i]+1);
+    setII.push_back(y);
+  }
+
+  MinAggregator min;
+  feature_array vectorI = min.vector_valued(setI);
+  feature_type scalarI = min.scalar_valued(setI);
+
+  feature_array vectorII = min.vector_valued(setII);
+  feature_type scalarII = min.scalar_valued(setII);
+
+  feature_array vectorIref(2); vectorIref[0] = 0.; vectorIref[1] = 1.;
+  feature_type scalarIref = 0.;
+
+  feature_array vectorIIref(1); vectorIIref[0] = 0.;
+  feature_type scalarIIref = 0.;
+
+  BOOST_CHECK_EQUAL(vectorIref[0], vectorI[0]);
+  BOOST_CHECK_EQUAL(vectorIref[1], vectorI[1]);
+  BOOST_CHECK_EQUAL(scalarIref, scalarI);
+  
+  BOOST_CHECK_EQUAL(vectorIIref[0], vectorII[0]);
+  BOOST_CHECK_EQUAL(scalarIIref, scalarII);
+
+}
+
+BOOST_AUTO_TEST_CASE( MaxAggregator_test ) {
+  // Set up some test data
+  feature_type setI_array[3][2] = {
+    {0., 1.},
+    {4., 8.},
+    {2., 3.}
+  };
+  feature_arrays setI;
+  for(size_t i = 0; i < 3; i++) {
+    feature_array y(setI_array[i], setI_array[i]+2);
+    setI.push_back(y);
+  }
+  feature_type setII_array[2][1] = {{0}, {2}};
+  feature_arrays setII;
+  for(size_t i = 0; i < 2; i++) {
+    feature_array y(setII_array[i], setII_array[i]+1);
+    setII.push_back(y);
+  }
+
+  MaxAggregator max;
+  feature_array vectorI = max.vector_valued(setI);
+  feature_type scalarI = max.scalar_valued(setI);
+
+  feature_array vectorII = max.vector_valued(setII);
+  feature_type scalarII = max.scalar_valued(setII);
+
+  feature_array vectorIref(2); vectorIref[0] = 4.; vectorIref[1] = 8.;
+  feature_type scalarIref = 8.;
+
+  feature_array vectorIIref(1); vectorIIref[0] = 2.;
+  feature_type scalarIIref = 2.;
+
+  BOOST_CHECK_EQUAL(vectorIref[0], vectorI[0]);
+  BOOST_CHECK_EQUAL(vectorIref[1], vectorI[1]);
+  BOOST_CHECK_EQUAL(scalarIref, scalarI);
+  
+  BOOST_CHECK_EQUAL(vectorIIref[0], vectorII[0]);
+  BOOST_CHECK_EQUAL(scalarIIref, scalarII);
+
+}
+
