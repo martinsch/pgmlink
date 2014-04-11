3fa6f79c258a0c719d7af37f1138a95da43b0ef6
martinsch <martin.schiegg@iwr.uni-heidelberg.de>
2014-04-11 15:21:34 +0200

    adjusted ilastik build script

--- build_binaries/install.sh
+++ build_binaries/install.sh
@@ -1,17 +1,19 @@
-ILASTIK_BUILD=$PWD
+ILASTIK_BUILD=$(dirname $0)/ilastik-build
 CPLEX_ROOT_DIR=$HOME/local
 
 sudo apt-get install build-essential g++ gcc git cmake cmake-curses-gui gfortran libxext-dev libfontconfig1-dev&&
 
-mkdir -p ilastik-build/build &&
-cd ilastik-build &&
+mkdir -p $ILASTIK_BUILD/build &&
+cp run_ilastik.sh $ILASTIK_BUILD/build &&
 
-cmake -D BUILDEM_DIR=$ILASTIK_BUILD/build -D CMAKE_CXX_COMPILER=g++ -D CMAKE_C_COMPILER=gcc -D CMAKE_CPLEX_ROOT_DIR=$CPLEX_ROOT_DIR $ILASTIK_BUILD &&
-
-make &&
+cd $ILASTIK_BUILD &&
 
 BUILDEM_DIR=$ILASTIK_BUILD/build 
 
-cmake -D BUILDEM_DIR=$ILASTIK_BUILD/build $ILASTIK_BUILD &&
+cmake -D BUILDEM_DIR=$BUILDEM_DIR -D CMAKE_CXX_COMPILER=g++ -D CMAKE_C_COMPILER=gcc -D CMAKE_CPLEX_ROOT_DIR=$CPLEX_ROOT_DIR $ILASTIK_BUILD/.. &&
 
-make -j8
+make &&
+
+cmake -D BUILDEM_DIR=$ILASTIK_BUILD/build $ILASTIK_BUILD/.. &&
+
+make -j
