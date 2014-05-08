50c9d4eb2beda14c6f5882f26d10a0d713661bac
dstoe <stockel.david@gmail.com>
2014-05-08 18:15:59 +0200

    cleaning up: removed needless code

--- include/pgmlink/higher_order_features.h
+++ include/pgmlink/higher_order_features.h
@@ -28,14 +28,9 @@
 
 namespace pgmlink {
 
-// forward declaration of class Track
-class Track;
-
 /*=============================================================================
  type definitions
 =============================================================================*/
-typedef std::vector<Traxel> Traxelvector;
-typedef std::vector<Track> Trackvector;
 typedef std::vector<HypothesesGraph::Node> Nodevector;
 typedef std::vector<const Traxel*> ConstTraxelRefVector;
 
@@ -46,88 +41,13 @@ typedef vigra::MultiArray<2, feature_type> FeatureMatrix;
 typedef vigra::MultiArrayView<1, feature_type> FeatureVectorView;
 
 /*=============================================================================
- function definitions
+ functions
 =============================================================================*/
 void set_solution(HypothesesGraph& graph, const size_t solution_index);
 
 /*=============================================================================
- class definitions
-=============================================================================*/
-////
-//// class Track
-////
-class Track {
- public:
-  Track();
-  Track(
-    const size_t id,
-    const size_t time_start,
-    const Traxelvector& traxels,
-    Track* const parent = NULL,
-    Track* const left_child = NULL,
-    Track* const right_child = NULL
-  );
-  void set_id(const size_t id);
-  void set_time_start(const size_t time_start);
-  void set_parent(Track* const parent);
-  void set_child(Track* const child);
-  void set_children(Track* const left_child, Track* const right_child);
-  size_t get_id() const;
-  size_t get_time_start() const;
-  Track* get_parent() const;
-  const std::vector<Track*>& get_children() const;
-  size_t get_length() const;
-
-  size_t id_;
-  size_t time_start_;
-  Traxelvector traxels_;
-  Track* parent_;
-  std::vector<Track*> children_;
- private:
-  friend class boost::serialization::access;
-  template<class Archive>
-  void serialize(Archive & archive, const unsigned int version) {
-    (void) version;
-    archive & id_;
-    archive & time_start_;
-    archive & traxels_;
-  }
-}; // class Track
-
-////
-//// class Tracking
-////
-class Tracking{
- public:
-  Tracking();
-  Tracking(const HypothesesGraph& graph, const size_t index);
-
-  Trackvector tracks_;
-  size_t index_;
- private:
-  friend class boost::serialization::access;
-  template<class Archive>
-  void serialize(Archive & archive, const unsigned int version) {
-    (void) version;
-    archive & tracks_;
-    archive & index_;
-  }
-}; // class Tracking
-
-/*=============================================================================
   pure virtual classes
 =============================================================================*/
-//// TODO: deprecated / use FeatureExtractor
-//// class TrackFeatureExtractor
-////
-class TrackFeatureExtractor {
- public:
-  TrackFeatureExtractor() {};
-  virtual ~TrackFeatureExtractor() {};
-  virtual const std::string& name() const = 0;
-  virtual const feature_arrays& operator()(const Track& track) = 0;
-};
-
 ////
 //// class FeatureAggregator
 ////
@@ -202,47 +122,8 @@ class OutlierCalculator {
 }; // class OutlierCalculator
 
 /*=============================================================================
-  specific functors
+  specific classes
 =============================================================================*/
-////
-//// class TrackValue
-////
-class TrackValue {
- public:
-  TrackValue(
-    TrackFeatureExtractor* track_feature_extractor,
-    FeatureAggregator* feature_aggregator
-  ) :
-    track_feature_extractor_(track_feature_extractor),
-    feature_aggregator_(feature_aggregator)
-  {};
-  const feature_type& operator()(const Track& track);
- protected:
-  TrackFeatureExtractor* track_feature_extractor_;
-  FeatureAggregator* feature_aggregator_;
-  feature_type ret_;
-};
-
-////
-//// class TrackingValue
-////
-class TrackingValue {
- public:
-  TrackingValue(
-    SubsetsOfInterest* subsets_of_interest,
-    SubsetFeatureExtractor* subset_feature_extractor,
-    SubsetFeatureAggregator* subset_feature_aggregator,
-    FeatureAggregator* feature_aggregator
-  );
-  const feature_type& operator()(const Tracking& tracking);
- protected:
-  SubsetsOfInterest* subsets_of_interest_;
-  SubsetFeatureExtractor* subset_feature_extractor_;
-  SubsetFeatureAggregator* subset_feature_aggregator_;
-  FeatureAggregator* feature_aggregator_;
-  feature_type ret_;
-};
-
 ////
 //// class SubsetFeaturesIdentity
 ////
@@ -260,38 +141,6 @@ class SubsetFeaturesIdentity : public SubsetFeatureExtractor {
   std::vector<std::string> feature_names_;
   FeatureMatrix ret_matrix_;
 }; // class SubsetFeaturesIdentity
-
-////
-//// class TrackFeaturesDiff
-////
-class TrackFeaturesDiff : public TrackFeatureExtractor {
- public:
-  TrackFeaturesDiff(const std::vector<std::string>& feature_names);
-  TrackFeaturesDiff(const std::string& feature_name);
-  virtual ~TrackFeaturesDiff() {};
-  virtual const std::string& name() const;
-  virtual const feature_arrays& operator()(const Track& track);
- protected:
-  static const std::string name_;
-  std::vector<std::string> feature_names_;
-  feature_arrays ret_;
-};
-
-////
-//// class TrackFeaturesCurvature
-////
-class TrackFeaturesCurvature : public TrackFeatureExtractor {
- public:
-  TrackFeaturesCurvature(const std::vector<std::string>& feature_names);
-  TrackFeaturesCurvature(const std::string& feature_name);
-  virtual ~TrackFeaturesCurvature() {};
-  virtual const std::string& name() const;
-  virtual const feature_arrays& operator()(const Track& track);
- protected:
-  static const std::string name_;
-  std::vector<std::string> feature_names_;
-  feature_arrays ret_;
-};
 
 ////
 //// class SumAggregator
@@ -356,47 +205,6 @@ class DivisionSubsets : public SubsetsOfInterest {
   static const std::string name_;
   std::vector<ConstTraxelRefVector> ret_;
 };
-
-////
-//// class SubsetFeatureExtractorFromFE
-////
-class SubsetFeatureExtractorFromFE : public SubsetFeatureExtractor {
- public:
-  SubsetFeatureExtractorFromFE(
-    TrackFeatureExtractor* track_feature_extractor
-  ) : track_feature_extractor_(track_feature_extractor) {};
-  virtual ~SubsetFeatureExtractorFromFE() {};
-  virtual const std::string& name() const;
-  virtual const feature_arrays& operator()(
-    const Trackvector& tracks
-  );
- protected:
-  static const std::string name_;
-  TrackFeatureExtractor* track_feature_extractor_;
-  feature_arrays ret_;
-};
-
-////
-//// class DivisionFeatureExtractor
-////
-// class DivisionFeatureExtractor : public SubsetFeatureExtractor {
-//  public:
-//   DivisionFeatureExtractor(const std::string& feature_name, size_t depth=1);
-//   DivisionFeatureExtractor(
-//     const std::vector<std::string>& feature_names,
-//     size_t depth=1
-//   );
-//   virtual ~DivisionFeatureExtractor() {};
-//   virtual const std::string& name() const;
-//   virtual const feature_arrays& operator()(
-//     const Trackvector& tracks
-//   );
-//  protected:
-//   static const std::string name_;
-//   TrackFeaturesIdentity features_identity_;
-//   size_t depth_;
-//   feature_arrays ret_;
-// };
 
 ////
 //// class SubsetFeatureAggregatorFromFA
