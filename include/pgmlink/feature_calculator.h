c87014990b2be7fc824c5da2d187560c37625ead
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-05-15 09:44:43 -0400

    added a comment

--- include/pgmlink/feature_calculator.h
+++ include/pgmlink/feature_calculator.h
@@ -1,5 +1,8 @@
 #ifndef FEATURE_CALCULATOR_H
 #define FEATURE_CALCULATOR_H
+
+// proxy header for including all (implemented) calculators
+
 // pgmlink
 #include "pgmlink/feature_calculator/base.h"
 #include "pgmlink/feature_calculator/absolute_difference.h"
@@ -10,8 +13,6 @@
 #include "pgmlink/feature_calculator/ratio.h"
 #include "pgmlink/feature_calculator/squared_difference.h"
 #include "pgmlink/feature_calculator/square_root_squared_difference.h"
-
-
 
 
 
