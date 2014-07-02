39e207253a5b08712c183d55bece14284071a094
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2014-07-02 19:38:12 +0200

    more Visual Studio compatibility fixes

--- include/pgmlink/windows.h
+++ include/pgmlink/windows.h
@@ -5,6 +5,19 @@
 // badly named Windows macros
 
 #if defined(_WIN32)
+# define VC_EXTRALEAN
+# ifndef NOMINMAX
+#  define NOMINMAX
+#  define _PGMLINK_UNDEFINE_NOMINMAX
+# endif
+# include <windows.h>
+# ifdef _PGMLINK_UNDEFINE_NOMINMAX
+#  undef NOMINMAX
+#  undef _PGMLINK_UNDEFINE_NOMINMAX
+# endif
+# ifdef DIFFERENCE
+#  undef DIFFERENCE
+# endif
 # ifdef IN
 #  undef IN
 # endif
