622d7f64b1762831f3fe0257a3be7d3b926fca7b
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2014-08-07 12:58:41 +0200

    removed nearest neighbor stuff

--- include/pgmlink/tracking.h
+++ include/pgmlink/tracking.h
@@ -74,63 +74,6 @@ namespace pgmlink {
     shared_ptr<std::vector< std::map<unsigned int, bool> > > last_detections_;
   };
 
-  class NNTracking {
-    public:
-      NNTracking(double divDist = 30,
-                 double movDist = 10,
-                 std::vector<std::string> features = std::vector<std::string>(0),
-                 double divisionThreshold = 0.5,
-                 bool splitterHandling = true,
-                 bool mergerHandling = true,
-                 std::vector<int> maxTraxelIdAt = std::vector<int>(0))
-
-        : divDist_(divDist), movDist_(movDist), distanceFeatures_(features),
-          divisionThreshold_(divisionThreshold), splitterHandling_(splitterHandling),
-          mergerHandling_(mergerHandling), maxTraxelIdAt_(maxTraxelIdAt){}
-      std::vector< std::vector<Event> > operator()(TraxelStore&);
-
-      /**
-       * Get state of detection variables after call to operator().
-       */
-      std::vector< std::map<unsigned int, bool> > detections();
-
-    private:
-      double divDist_, movDist_;
-      std::vector<std::string> distanceFeatures_;
-      double divisionThreshold_;
-      bool splitterHandling_, mergerHandling_;
-      shared_ptr<std::vector< std::map<unsigned int, bool> > > last_detections_;
-      std::vector<int> maxTraxelIdAt_;
-    };
-
-
-  class NNTrackletsTracking {
-      public:
-	  NNTrackletsTracking(double maxDist = 30,
-                              std::vector<std::string> features = std::vector<std::string>(0),
-                              double divisionThreshold = 0.5,
-                              bool splitterHandling = true,
-                              bool mergerHandling = true,
-                              std::vector<int> maxTraxelIdAt = std::vector<int>(0))
-          : maxDist_(maxDist), distanceFeatures_(features),
-            divisionThreshold_(divisionThreshold), splitterHandling_(splitterHandling),
-            mergerHandling_(mergerHandling), maxTraxelIdAt_(maxTraxelIdAt){}
-        std::vector< std::vector<Event> > operator()(TraxelStore&);
-
-        /**
-         * Get state of detection variables after call to operator().
-         */
-        std::vector< std::map<unsigned int, bool> > detections();
-
-      private:
-        double maxDist_;
-        std::vector<std::string> distanceFeatures_;
-        double divisionThreshold_;
-        bool splitterHandling_, mergerHandling_;
-        shared_ptr<std::vector< std::map<unsigned int, bool> > > last_detections_;
-        std::vector<int> maxTraxelIdAt_;
-  };
-
   class ConsTracking {
     public:
 	  ConsTracking(
