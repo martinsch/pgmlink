8e87986433a26c2d1880f3ed43999b6ac32a633e
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2014-08-07 15:41:15 +0200

    run_test_sequence.cmd: pythonpath instructions

--- example/run_test_sequence.cmd
+++ example/run_test_sequence.cmd
@@ -1,4 +1,6 @@
 #! /bin/bash
+# The PYTHONPATH must point to the ilastik source folder and the installed pgmlink library
+
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
 python ${SCRIPT_DIR}/../workflow/workflow.py ${SCRIPT_DIR}/testData/Sequence000-003_probabilities.h5 -r ${SCRIPT_DIR}/testData/Sequence000-003.h5 --prediction-channel=1 --normalize-predictions --sigma=0.3 --background-threshold=128 --t-axis=0 --ch-axis=3 --percentile=0.8 --progressive=0.9 --num-levels=4 --detectionClassifier=${SCRIPT_DIR}/testData/ilastik-object-detection.ilp --seedRadius=1 --classifierFile=${SCRIPT_DIR}/testData/classifiers.h5 --output=${SCRIPT_DIR}/result.h5
