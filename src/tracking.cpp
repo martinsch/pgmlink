e46aaf4526ec23bde79d699252245eff3599aab3
Carsten Haubold <carstenhaubold@googlemail.com>
2015-06-24 10:44:58 +0200

    Use OpenGM's new guard to include LPDef symbols only once

--- src/tracking.cpp
+++ src/tracking.cpp
@@ -10,6 +10,10 @@
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
 
+// include the LPDef symbols only once!
+#undef OPENGM_LPDEF_NO_SYMBOLS
+#include <opengm/inference/auxiliary/lpdef.hxx>
+
 #include "pgmlink/feature.h"
 #include "pgmlink/pgm.h"
 #include "pgmlink/hypotheses.h"
@@ -18,7 +22,6 @@
 #include "pgmlink/tracking.h"
 #include "pgmlink/reasoner_constracking.h"
 #include "pgmlink/merger_resolving.h"
-
 
 using namespace std;
 using boost::shared_ptr;
